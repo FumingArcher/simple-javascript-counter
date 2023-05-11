a4143215e6135682a80f5e7325918986a55a4df1
FumingArcher <133020515+FumingArcher@users.noreply.github.com>
2023-05-11 22:30:10 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+let count = 0;
+
+document.getElementById("inc").onclick= function(){
+
+   count+=1;
+
+   document.getElementById("countlabel").innerHTML = count;
+}
+
+document.getElementById("res").onclick= function(){
+
+count=0;
+
+document.getElementById("countlabel").innerHTML = count;
+}
+
+document.getElementById("dec").onclick= function(){
+
+count-=1;
+
+document.getElementById("countlabel").innerHTML = count;
+
+}
